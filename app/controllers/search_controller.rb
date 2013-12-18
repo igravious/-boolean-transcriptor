f58e99eb82caaa7c2bebd2e4c555e93d765910f0
Anthony Durity <anthony@durity.com>
2013-12-18 08:20:28 +0000

    Making Search Way More Awesome

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,5 +2,6 @@ class SearchController < ApplicationController
 
     def index
         @elapsed_time_in_secs = Benchmark.realtime { @items = Item.find(:all, :conditions => ['description LIKE ?', "%#{params['q']}%"]) }
+        @elapsed_time_in_secs += Benchmark.realtime { @scans = Scan.find(:all, :conditions => ['transcription LIKE ?', "%#{params['q']}%"]) }
     end
 end
